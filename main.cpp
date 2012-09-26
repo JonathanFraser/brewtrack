af57d44760722a6174eb0f4767020a8446155f45
Jonathan Fraser <jonathanfras@gmail.com>
2012-09-25 21:01:50 -0700

    added some batches in the test main added the database to .gitignore

--- main.cpp
+++ main.cpp
@@ -10,5 +10,18 @@ int main(void) {
 auto datasource = make_shared<SQLiteSource>("testfile.dat");
 Database base;
 base.setDataSource(datasource);
+BatchRef batch1 = make_shared<Batch>();
+batch1->setName("Batch1");
+batch1->setDescription("Description of Batch1");
+base.addBatch(batch1);
+BatchRef batch2 = make_shared<Batch>();
+batch2->setName("Batch2");
+batch2->setDescription("Description of Batch2");
+base.addBatch(batch2);
+BatchRef batch3 = make_shared<Batch>();
+batch3->setName("Batch3");
+batch3->setDescription("Description of Batch3");
+base.addBatch(batch3);
+
 return 0;
 }
