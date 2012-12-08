b23ff051ef51a7537735f83ce87ecdf23d83160e
Jonathan Fraser <jonathanfras@gmail.com>
2012-12-08 13:40:13 -0800

    remove sqlite reference and using tnt db

--- sqlite3/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "sqlite3pp.h"
-
-int main(void) {
-	Sqlite3pp::Connection conn("Test.dat");
-	return 0;
-}
--- sqlite3/sqlite3pp.h
+++
@@ -1,7 +0,0 @@
-#ifndef SQLITE3PP_H_
-#define SQLITE3PP_H_
-
-#include "rawtypes.h"
-#include "connection.h"
-
-#endif
